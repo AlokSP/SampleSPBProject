3dffa73728ccce2104cde77a842da2c568ff9b38
Alok Kumar <alokspmuz143@gmail.com>
2022-12-01 12:58:27 +0530

    2nd commit

--- src/main/java/com/example/demo/SampleProjectApplication.java
+++ src/main/java/com/example/demo/SampleProjectApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleProjectApplication {
+public class SampleProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+		
 	}
 
 }
