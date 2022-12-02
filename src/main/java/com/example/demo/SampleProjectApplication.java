246b077feb287a51ea21df344df7c7752d9e7bed
Alok Kumar <alokspmuz143@gmail.com>
2022-12-02 11:35:47 +0530

    4rth commit

--- src/main/java/com/example/demo/SampleProjectApplication.java
+++ src/main/java/com/example/demo/SampleProjectApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,9 @@ public class SampleProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
+		/*SpringApplication app=new SpringApplication(SampleProjectApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args); */
 		System.out.println("Hello World");
 	}
 
